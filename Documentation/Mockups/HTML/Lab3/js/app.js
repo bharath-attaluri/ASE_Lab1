0b0b613c13333efc84c70c36a456231f63bdd576
bharathattaluri <saqn5@mail.umkc.edu>
2015-09-26 19:54:26 -0500

    Lab4

--- Documentation/Mockups/HTML/Lab3/js/app.js
+++ Documentation/Mockups/HTML/Lab3/js/app.js
@@ -3,3 +3,9 @@ var AddressBook = angular.module('AddressBook', []);
 AddressBook.controller('PersonController', function ($scope) {
   
 });
+
+var app = angular.module('myApp', []);
+
+app.controller('PersonController', function ($scope) {
+   
+});
